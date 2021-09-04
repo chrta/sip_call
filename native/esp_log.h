b9caee2697432274cd014407d22fd93bdd289f5e
Christian Taedcke <hacking@taedcke.com>
2021-09-04 15:22:00 +0200

    native: Fix log level string

--- native/esp_log.h
+++ native/esp_log.h
@@ -14,8 +14,8 @@ static inline void my_log(const char* level, const char* prefix, const char* fmt
     printf("\n");
 }
 
-#define ESP_LOGE(prefix, fmt, ...) my_log("[ERR] ", prefix, fmt, ##__VA_ARGS__)
-#define ESP_LOGW(prefix, fmt, ...) my_log("[WARN] ", prefix, fmt, ##__VA_ARGS__)
-#define ESP_LOGI(prefix, fmt, ...) my_log("[INFO] ", prefix, fmt, ##__VA_ARGS__)
-#define ESP_LOGD(prefix, fmt, ...) my_log("[DBG] ", prefix, fmt, ##__VA_ARGS__)
-#define ESP_LOGV(prefix, fmt, ...) my_log("[VER] ", prefix, fmt, ##__VA_ARGS__)
+#define ESP_LOGE(prefix, fmt, ...) my_log("ERR", prefix, fmt, ##__VA_ARGS__)
+#define ESP_LOGW(prefix, fmt, ...) my_log("WARN", prefix, fmt, ##__VA_ARGS__)
+#define ESP_LOGI(prefix, fmt, ...) my_log("INFO", prefix, fmt, ##__VA_ARGS__)
+#define ESP_LOGD(prefix, fmt, ...) my_log("DBG", prefix, fmt, ##__VA_ARGS__)
+#define ESP_LOGV(prefix, fmt, ...) my_log("VER", prefix, fmt, ##__VA_ARGS__)
