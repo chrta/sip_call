95982fb55bd9c3d75349f8ac888bdc4c398326ac
Christian Taedcke <hacking@taedcke.com>
2026-02-15 21:46:16 +0100

    Update to esp-idf v5.5.2

--- ci/static_code_analysis.sh
+++ ci/static_code_analysis.sh
@@ -6,6 +6,6 @@ set -eo pipefail
 
 mkdir build_clang
 cd build_clang
-CC=clang-15 CXX=clang++-15 cmake ../native -G Ninja -D CMAKE_CXX_FLAGS=-DASIO_STANDALONE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+CC=clang-20 CXX=clang++-20 cmake ../native -G Ninja -D CMAKE_CXX_FLAGS=-DASIO_STANDALONE -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-run-clang-tidy-15.py -clang-tidy-binary clang-tidy-15
+run-clang-tidy-20.py -clang-tidy-binary clang-tidy-20
